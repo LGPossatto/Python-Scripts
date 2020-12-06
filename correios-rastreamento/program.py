05e111b65ed493082c74cf013ae899c45f0eb345
Luiz Gustavo <lgpossatto@gmail.com>
2020-12-06 18:47:35 -0300

    correios-rastreamento added comments

--- correios-rastreamento/program.py
+++ correios-rastreamento/program.py
@@ -7,17 +7,22 @@ from notify import notification
 tracking_code = 'your code'
 url = 'https://www2.correios.com.br/sistemas/rastreamento/default.cfm'
 
+#make browser invisible
 options = Options()
 options.add_argument('--headless')
 
+#open firefox in the selected url
 driver = webdriver.Firefox(options=options)
 driver.get(url)
 
+#add the tracking code to the form box and then click the submit button
 driver.find_element_by_id('objetos').send_keys(tracking_code)
 driver.find_element_by_id('btnPesq').click()
 
+#now in the new page select the wanted element
 element_text = driver.find_element_by_id('UltimoEvento').text
 
+#organize the text to print with notify
 txt_status = ''
 txt_date = ''
 for i in element_text:
@@ -30,4 +35,5 @@ for i in element_text:
 
 notification(f'Estado: {txt_status}\nData da atualização: {txt_date[1:]}', title='Estado da encomenda')
 
+#close
 driver.quit()
